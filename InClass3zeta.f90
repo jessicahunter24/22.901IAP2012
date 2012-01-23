b85843c2be02b39e5b4e768bf4d2d15e6bf1cbab
Jessica <jessicahunter24@gmail.com>
2012-01-23 15:44:47 -0500

    Adding class 3 files

---
+++ InClass3zeta.f90
@@ -0,0 +1,25 @@
+program zeta
+
+implicit none
+
+
+end program zeta
+
+
+
+subroutine zeta_sub()
+
+implicit none
+
+
+end subroutine zeta_sub
+
+
+
+function zeta_fun()
+
+implicit none
+real :: zeta_fun
+
+
+end function zeta_fun()
---
+++ endass3.f90
@@ -0,0 +1,9 @@
+program endass3
+
+implicit none
+
+
+
+  STOP
+
+end program endass3
